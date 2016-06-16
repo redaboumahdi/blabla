2f5817e2244d005bc98c4afc1742c2abf8077bbc
redaboumahdi <reda.boumahdi@polytechnique.edu>
2016-06-16 20:01:46 +0100

    nimp git commit

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+  $HOST = 'localhost';
+  $USER='ondagaia_reda';
+  $PASS = 'reda1992';
+  $DB='ondagaia_data';
+  
+  $con = mysqli_connect($HOST,$USER,$PASS,$DB);
+// Check connection
+
+
+?>
